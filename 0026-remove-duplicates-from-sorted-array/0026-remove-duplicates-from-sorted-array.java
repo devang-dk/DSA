dd93da701f31dc3aa8ead0a9ca2fc283586b24d3
Devang Khandelwal <141030940+devang-dk@users.noreply.github.com>
2026-08-07 16:52:26 +0530

    Time: 1 ms (77.27%), Space: 46.9 MB (24.92%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=0;
+        int j=1;
+        int k=1;
+        while(j<nums.length){
+            if(nums[i]==nums[j]){
+                j++;
+            }
+            else{
+                nums[k]=nums[j];
+                i=j;
+                j++;
+                k++;
+            }
+        }
+        return k;
+    }
+}
