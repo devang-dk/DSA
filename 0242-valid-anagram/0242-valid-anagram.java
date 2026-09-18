efc2090f5df1d13605b311f83c5fd96637a75d7f
Devang Khandelwal <khandelwaldevang573@gmail.com>
2026-09-18 10:26:28 +0530

    Time: 16 ms (20.9%), Space: 46.5 MB (24.65%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,12 +3,10 @@ class Solution {
         HashMap<Character,Integer> map  = new HashMap<>();
         HashMap<Character,Integer> map1  = new HashMap<>();
         for(int i=0;i<s.length();i++){
-            char c = s.charAt(i);
-            map.put(c, map.getOrDefault(c,0)+1);
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
         }
         for(int i=0;i<t.length();i++){
-            char c = t.charAt(i);
-            map1.put(c, map1.getOrDefault(c,0)+1);
+            map1.put(t.charAt(i), map1.getOrDefault(t.charAt(i),0)+1);
         }
 
         return map.equals(map1);
