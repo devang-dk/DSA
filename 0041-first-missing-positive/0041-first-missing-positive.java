2bf1aecaa44ec8d2a59236e4feaa5083b20bd6d5
Devang Khandelwal <khandelwaldevang573@gmail.com>
2026-09-19 10:35:10 +0530

    Time: 17 ms (17.13%), Space: 92.7 MB (9.73%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,17 +1,17 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int max=0;
         HashMap<Integer,Integer> ans = new HashMap<>();
         for(int c:nums){
             ans.put(c, ans.getOrDefault(c,0)+1);
-            max = Math.max(max,c);
         }
-        // if(max=0)
-        for(int i=1;i<=max;i++){
+        int last=0;
+        for(int i=1;i<nums.length+1;i++){
             if(!ans.containsKey(i)){
                 return i;
+            }else{
+                last=i;
             }
         }
-        return max+1;
+        return last+1;
     }
 }
